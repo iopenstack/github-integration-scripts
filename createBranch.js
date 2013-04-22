2cae431934601cde631e403c8585b289f6a986da
Chris Cantu <christopher.cantu@gmail.com>
2013-04-22 13:53:39 -0500

    Fixed poorly implemented code

--- createBranch.js
+++ createBranch.js
@@ -2,33 +2,26 @@ var Q = require('Q');
 var config = require('./configuration.js').config;
 
 var rest = require('restler');
-var defer = Q.defer();
-
 var branchName = process.argv[2];  //THE BRANCH NAME MUST BE INCLUDED AS AN ARGUMENT
-
-var resolveData = function(data){
-	defer.resolve(data);
-	defer.promise;
-	return data;
-}
-
-var handleError = function(error){
-	console.log("An Error occurred: '" + error.message = "'");
-	defer.resolve(error);
-	defer.promise;
-	return error;
-}
 
 return Q.fcall(function () {
 	console.log("Retrieving Commits")
+	
+	var defer = Q.defer();
+
 	rest.get('https://api.github.com/repos/' + config.owner + '/' + config.repoName + '/commits', 
 			{ headers: config.theHeaders }
-		).on('success', resolveData).on('fail', handleError);
-}).then( function() {
+		).on('success', function (data) {
+			defer.resolve(data);
+		}).on('fail', function (error) {
+			defer.reject(error);
+		});
 	return defer.promise;
+
 }).then( function(commits) {
 	console.log("Creating New Branch: " + branchName);
-
+	
+	var defer = Q.defer();
 	var lastCommit = commits[0];
 	var ref = "refs/heads/" + branchName;
     var data = JSON.stringify({ref: ref, sha: lastCommit.sha });
@@ -36,9 +29,13 @@ return Q.fcall(function () {
     rest.post('https://api.github.com/repos/' + config.owner + '/' + config.repoName + '/git/refs', {
     	headers: config.theHeaders, 
     	data: data
-    }).on('success', resolveData).on('fail', handleError);
+    }).on('success', function (data){
+    	defer.resolve(data);
+    }).on('fail', function (error){
+    	defer.resolve(error);
+    });
+
+    return defer.promise;
 }).then( function(){
-	return defer.promise
-}).then( function(){
-	console.log("Branch Created");
+	console.log("Branch '" + branchName + "' Created");
 });
