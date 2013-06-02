5436c45a9e281cf7aec50734baed992a0df90cbb
Chris Cantu <christopher.cantu@gmail.com>
2013-06-02 00:20:59 -0500

    Finished tweeks for createPullRequest.js script

--- createPullRequest.js
+++ createPullRequest.js
@@ -7,9 +7,8 @@ return Q.fcall( function () {
     console.log("Creating Pull Request for branch '" + branchName + "'");
 
     var defer = Q.defer(), data = {
-        "title": "Amazing new feature",
-        "body": "Please pull this in!",
-        "head": "christophercantu:" + branchName,
+        "title": branchName,
+        "head":  branchName,
         "base": "master"
     }, json = JSON.stringify(data);
 
@@ -17,6 +16,7 @@ return Q.fcall( function () {
             { headers: config.theHeaders, data: json }
         ).on('success', function (data) {
         defer.resolve(data);
+        console.log("Success: Created a new Pull Request for branch: '" + branchName + "'");
     }).on('fail', function (error) {
         console.log(error)
         defer.reject(error);
