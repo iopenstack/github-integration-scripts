d81f269652a26e49ec932b89fdc188ea82ada7cc
Chris Cantu <christopher.cantu@gmail.com>
2013-06-02 00:00:04 -0500

    Added new createPullReqeust script

---
+++ createPullRequest.js
@@ -0,0 +1,26 @@
+var Q = require('Q'), rest = require('restler'), _ = require('lodash');
+var config = require('./configuration.js').config;
+
+var branchName = process.argv[2];  //THE BRANCH NAME MUST BE INCLUDED AS AN ARGUMENT
+
+return Q.fcall( function () {
+    console.log("Creating Pull Request for branch '" + branchName + "'");
+
+    var defer = Q.defer(), data = {
+        "title": "Amazing new feature",
+        "body": "Please pull this in!",
+        "head": branchName,
+        "base": "master"
+    }, json = JSON.stringify(data);
+
+    rest.post('https://api.github.com/repos/' + config.owner + '/' + config.repoName + '/pulls',
+            { headers: config.theHeaders, data: json }
+        ).on('success', function (data) {
+        defer.resolve(data);
+    }).on('fail', function (error) {
+        console.log(error)
+        defer.reject(error);
+    });
+
+	return defer.promise;
+}).done();
